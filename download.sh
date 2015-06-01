23f07b50eca9dffbb0f6f8eb03f79b0c805179f5
Mario Wenzel <maweki@gmail.com>
2015-06-01 20:55:06 +0200

    close vlc after converting

--- download.sh
+++ download.sh
@@ -10,6 +10,6 @@ COMMAND="rtmpdump -q $RTMPLINE ${TMPDIR}${PROG}.flv"
 echo $COMMAND > ${TMPDIR}${PROG}.sh
 bash ${TMPDIR}${PROG}.sh
 rm ${TMPDIR}${PROG}.sh
-vlc -I dummy --sout "#transcode{acodec=mp3,ab=128,channels=2}:std{access=file,mux=raw,dst=${TMPDIR}${PROG}.mp3}" ${TMPDIR}${PROG}.flv
+vlc -I dummy --sout "#transcode{acodec=mp3,ab=128,channels=2}:std{access=file,mux=raw,dst=${TMPDIR}${PROG}.mp3}" ${TMPDIR}${PROG}.flv vlc://quit
 rm ${TMPDIR}${PROG}.flv
 mv ${TMPDIR}${PROG}.mp3 ${TARGET}${PROG}.mp3
